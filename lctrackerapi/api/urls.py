b88cf3e9937d972bd51797df2cd46573a5eaf351
alx-yng <alexyoung0119@gmail.com>
2024-04-13 17:43:36 -0500

    added custom endpoint for filtering notes

--- lctrackerapi/api/urls.py
+++ lctrackerapi/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('notes/', views.NoteListCreate.as_view(), name="note-list"),
     path('notes/<int:pk>/', views.NoteRetrieveUpdateDestroy.as_view(), name='note-select'),
+    path('notes/list/', views.NoteListView.as_view(), name='note-list-specific'),
 ]
--- lctrackerapi/api/views.py
+++ lctrackerapi/api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import generics
 from .models import Note
 from .serializers import NoteSerializer
+from rest_framework.views import APIView
 
 # Create your views here.
 # create a view that lists (using restframework) all note objects using the note serializer
@@ -9,7 +12,23 @@ class NoteListCreate(generics.ListCreateAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     
+    def delete(self, request, *args, **kwargs):
+        Note.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 class NoteRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     lookup_field = 'pk' # primary key
+    
+class NoteListView(APIView):
+    def get(self, request, format=None):
+        title = request.query_params.get('title', None)
+        
+        if title:
+            notes = Note.objects.filter(title_icontains=title)
+        else:
+            notes = Note.objects.all()
+            
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
