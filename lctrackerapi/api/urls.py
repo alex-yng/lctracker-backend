bd8dbb773c211d0251bacf0b09e08812ad6167a8
alx-yng <alexyoung0119@gmail.com>
2024-04-15 16:26:35 -0500

    fix: updated url to actually set the endpoint

--- lctrackerapi/api/urls.py
+++ lctrackerapi/api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('crud/<int:pk>/', views.NoteRetrieveUpdateDestroy.as_view(), name='note-crud'),
     path('notes/create/',views.MakeNote, name='note-create'),
     path('notes/',views.GetNotes, name='note-get'),
+    path('notes/<str:probID>/update/', views.UpdateNote, name='note-update'),
     path('notes/<str:probID>/delete/', views.DeleteNote, name='note-delete'),
 ]
